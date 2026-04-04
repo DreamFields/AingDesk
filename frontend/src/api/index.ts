71ef0de32488963947636913ba6280838ed64772
DreamFields <1339611623@qq.com>
2026-04-04 23:03:38 +0800

    fix:修复打包错误

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -44,7 +44,7 @@ instance.interceptors.response.use(
 export async function post<T>(
   url: string,
   params?: T
-): Promise<{ message: any; status: number; code: number; msg?: string; error_msg?: string; success?: boolean; credits?: number; plans?: any[]; models?: any[]; error?: string }> {
+): Promise<{ message: any; status: number; code: number; msg?: string; error_msg?: string; success?: boolean; credits?: number; plans?: any[]; models?: any[]; error?: string; count?: number }> {
   return instance.post(url, params)
 }
 
